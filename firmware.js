1c5e97d3c4abf140de95e5a39c4abcf33ad6caac
Den MacUndefined <denmacundefined@gmail.com>
2019-02-04 17:43:03 +0200

    added new features

--- firmware.js
+++ firmware.js
@@ -1,44 +1,67 @@
 function onInit() {
-  wifi = require('Wifi');
-  ssid = 'HOME';
-  password = 'd0m1nat0r';
-  port = 81;
-  http = require('http');
+  redpin = 33;
+  greenpin = 25;
+  bluepin = 26;
+  buzzerpin = 32;
+  co2pin = 35;
+  gaspin = 34;
+  buttonpin = 27;
+  relays = [4, 12, 13, 14, 2];
+  freePins = [0, 15];
+  i2cInit();
+  relaysInit();
+  connectWifi('HOME', 'd0m1nat0r');
+  serverInit();
+}
+
+function connectWifi(ssid, password) {
+   wifi = require('Wifi');
+   wifi.connect(ssid, {password: password}, function() {
+    console.log(`${wifi.getIP().ip}:81`);
+    display.setFontVector(12);
+    display.drawString((`${wifi.getIP().ip}:81`), 0, 0);
+    display.flip();
+  });
+}
+
+function i2cInit() {
   I2C1.setup({scl:22, sda:21});
-  display = require("SSD1306").connect(I2C1, start);
+  display = require("SSD1306").connect(I2C1);
   bmp = require("BMP085").connect(I2C1);
   htu = require('HTU21D').connect(I2C1);
 }
 
-function start() {
-  wifi.connect(ssid, {password: password}, function() {
-    console.log(`${wifi.getIP().ip}:${port}`);
-    display.setFontVector(12);
-    display.drawString((`${wifi.getIP().ip}:${port}`), 0, 0);
-    display.flip();
-    http.createServer(function(req, res) {
-      res.writeHead(200);
-      res.write('DenMacUndefined it\'s a legend');
-      bmp.getPressure(function(d) {
-        res.write("\nPressure: " + d.pressure + " Pa");
-        res.write("\nTemperature: " + d.temperature + " C");
-        htu.getTemperature( function(temp) {
-           res.write("\nTemperature 2: " + temp + " C");
-           htu.getHumidity( function(hum) {
-              res.write("\nHumidity: " + hum + " %");
-              res.write("\nCO2: " + analogRead(34) + " ppm");
-              res.write("\nGAS: " + analogRead(35) + " ppm");
-              res.end();
-           });
-         });
+function relaysInit() {
+  for (var i = 1; i < relays.length; i++) {
+    pinMode(relays[i], 'output');
+  }
+}
+
+function serverInit() {
+  http = require('http');
+  http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.write('DenMacUndefined it\'s a legend');
+    bmp.getPressure(function(d) {
+      res.write("\nPressure: " + d.pressure + " Pa");
+      res.write("\nTemperature: " + d.temperature + " C");
+      htu.getTemperature( function(temp) {
+        res.write("\nTemperature better: " + temp + " C");
+        htu.getHumidity( function(hum) {
+          res.write("\nHumidity: " + hum + " %");
+          res.write("\nCO2: " + analogRead(co2pin) + " ppm");
+          res.write("\nGAS: " + analogRead(gaspin) + " ppm");
+          res.write("\nButton: " + digitalRead(buttonpin));
+          analogWrite(buzzerpin, 1);
+          setTimeout(function() {
+            analogWrite(buzzerpin, 0);
+          }, 333);
+          res.end();
+        });
       });
-      digitalWrite(16, 1);
-      display.setFontVector(12);
-      display.drawString('request done', 0, 22);
-      display.flip();
-      setTimeout(() => {
-        digitalWrite(16, 0);
-      }, 3000);
-    }).listen(port);
-  });
+    });
+    display.setFontVector(12);
+    display.drawString('request done', 0, 22);
+    display.flip();
+  }).listen(81);
 }
